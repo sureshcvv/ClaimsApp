dbf5bfb8b023f1d8f71176ddae78fffae49a1524
hmalipatlolla <119707079+hmalipatlolla@users.noreply.github.com>
2022-12-16 10:02:32 -0600

    adding model

---
+++ Claims UI/src/app/components/model/claim.model.ts
@@ -0,0 +1,16 @@
+export class  Claim {
+    String_id: string;
+    claim_id: string;
+    facility_id: string;
+    pallet_quantity: number;
+    document_type: string;
+    claimed_amount: string;
+    service_provider_claim_id : number;
+    claim_status: string;
+    claim_type: string;
+    creator_id: string;
+    last_update_id: string;
+    closed_date: string;
+    create_date: string;
+    last_update_date: string;
+}
