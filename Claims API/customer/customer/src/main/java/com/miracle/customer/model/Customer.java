1ad61049058745422d46023569944743fe576b75
vivekreddy <vivekreddy.banala007@gmail.com>
2022-12-19 14:22:59 -0500

    dropdown changes

--- Claims API/customer/customer/src/main/java/com/miracle/customer/model/Customer.java
+++ Claims API/customer/customer/src/main/java/com/miracle/customer/model/Customer.java
@@ -17,228 +17,80 @@ import lombok.Setter;
 @Document(collection = "customer")
 @JsonInclude(value = Include.NON_NULL)
 public class Customer {
-	@Id
+@Id
 //	@Field("_id")
-	private String _id;
+private String _id;
 
-	@ApiModelProperty(required = true, value = "customer id", name = "customerId", dataType = "Integer", example = "1234")
-	@Field("INTERNAL_ID")
-	private Integer customerId;
-	
-	@ApiModelProperty(required = true, value = "name", name = "name", dataType = "String", example = "1234")
-	@Field("NAME")
-	private String name;
-	
-	@ApiModelProperty(required = true, value = "address line 1", name = "addressLine1", dataType = "String", example = "1415 North Raymond Avenue")
-	@Field("ADDRESS_LINE_1")
-	private String addressLine1;
+@ApiModelProperty(required = true, value = "customer id", name = "customerId", dataType = "Integer", example = "1234")
+@Field("customer_id")
+private Integer customerId;
 
-	@ApiModelProperty(required = false, value = "address line 2", name = "addressLine2", dataType = "String", example = "Suite 100")
-	@Field("ADDRESS_LINE_2")
-	private String addressLine2;
+@ApiModelProperty(required = true, value = "name", name = "name", dataType = "String", example = "1234")
+@Field("name")
+private String name;
 
-	@ApiModelProperty(required = false, value = "address line 3", name = "addressLine3", dataType = "String", example = "")
-	@Field("ADDRESS_LINE_3")
-	private String addressLine3;
-	
-	@ApiModelProperty(required = true, value = "city", name = "city", dataType = "String", example = "Anaheim")
-	@Field("CITY")
-	private String city;
-	
-	@ApiModelProperty(required = true, value = "state", name = "state", dataType = "String", example = "CA")
-	@Field("STATE")
-	private String state;
-	
-	@ApiModelProperty(required = true, value = "postal code", name = "postalCode", dataType = "String", example = "92801")
-	@Field("POSTAL_CODE")
-	private String postalCode;
-	
-	@ApiModelProperty(required = true, value = "country", name = "country", dataType = "String", example = "United States")
-	@Field("COUNTRY")
-	private String country;
+@ApiModelProperty(required = true, value = "address line 1", name = "addressLine1", dataType = "String", example = "1415 North Raymond Avenue")
+@Field("address_line_1")
+private String addressLine1;
 
-	@ApiModelProperty(required = true, value = "phone", name = "phone", dataType = "String", example = "(714) 449-2880")
-	@Field("PHONE")
-	private String phone;
+@ApiModelProperty(required = false, value = "address line 2", name = "addressLine2", dataType = "String", example = "Suite 100")
+@Field("address_line_2")
+private String addressLine2;
 
-	@ApiModelProperty(required = true, value = "fax", name = "fax", dataType = "String", example = "18473037674")
-	@Field("FAX")
-	private String fax;
-	
-	@ApiModelProperty(required = true, value = "email", name = "email", dataType = "String", example = "18473037674")
-	@Field("EMAIL")
-	private String email;
-	
-	@ApiModelProperty(required = true, value = "creatorId", name = "creatorId", dataType = "String", example = "18473037674")
-	@Field("CREATOR_ID")
-	private String creatorId;
-	
-	@ApiModelProperty(required = true, value = "lastUpdatorId", name = "lastUpdatorId", dataType = "String", example = "18473037674")
-	@Field("LAST_UPDATOR_ID")
-	private String lastUpdatorId;
-	
-	@ApiModelProperty(required = true, value = "createDate", name = "createDate", dataType = "String", example = "")
-	@Field("CREATE_DATE")
-	private String createDate;
-	
-	@ApiModelProperty(required = true, value = "lastUpdateDate", name = "lastUpdateDate", dataType = "String", example = "")
-	@Field("LAST_UPDATE_DATE")
-	private String lastUpdateDate;
+@ApiModelProperty(required = false, value = "address line 3", name = "addressLine3", dataType = "String", example = "")
+@Field("address_line_3")
+private String addressLine3;
 
-	public Integer getCustomerId() {
-		return customerId;
-	}
+@ApiModelProperty(required = true, value = "city", name = "city", dataType = "String", example = "Anaheim")
+@Field("city")
+private String city;
 
-	public void setCustomerId(Integer customerId) {
-		this.customerId = customerId;
-	}
+@ApiModelProperty(required = true, value = "state", name = "state", dataType = "String", example = "CA")
+@Field("state")
+private String state;
 
-	public String getName() {
-		return name;
-	}
+@ApiModelProperty(required = true, value = "postal code", name = "postalCode", dataType = "String", example = "92801")
+@Field("postal_code")
+private String postalCode;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+@ApiModelProperty(required = true, value = "country", name = "country", dataType = "String", example = "United States")
+@Field("country")
+private String country;
 
-	public String getAddressLine1() {
-		return addressLine1;
-	}
+@ApiModelProperty(required = true, value = "phone", name = "phone", dataType = "String", example = "(714) 449-2880")
+@Field("phone")
+private String phone;
 
-	public void setAddressLine1(String addressLine1) {
-		this.addressLine1 = addressLine1;
-	}
+@ApiModelProperty(required = true, value = "fax", name = "fax", dataType = "String", example = "18473037674")
+@Field("fax")
+private String fax;
 
-	public String getAddressLine2() {
-		return addressLine2;
-	}
+@ApiModelProperty(required = true, value = "email", name = "email", dataType = "String", example = "18473037674")
+@Field("email")
+private String email;
 
-	public void setAddressLine2(String addressLine2) {
-		this.addressLine2 = addressLine2;
-	}
+@ApiModelProperty(required = true, value = "creatorId", name = "creatorId", dataType = "String", example = "18473037674")
+@Field("creator_id")
+private String creatorId;
 
-	public String getAddressLine3() {
-		return addressLine3;
-	}
+@ApiModelProperty(required = true, value = "lastUpdatorId", name = "lastUpdatorId", dataType = "String", example = "18473037674")
+@Field("last_updator_id")
+private String lastUpdatorId;
 
-	public void setAddressLine3(String addressLine3) {
-		this.addressLine3 = addressLine3;
-	}
+@ApiModelProperty(required = true, value = "createDate", name = "createDate", dataType = "String", example = "")
+@Field("create_date")
+private String createDate;
 
-	public String getCity() {
-		return city;
-	}
+@ApiModelProperty(required = true, value = "lastUpdateDate", name = "lastUpdateDate", dataType = "String", example = "")
+@Field("last_update_date")
+private String lastUpdateDate;
 
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getPostalCode() {
-		return postalCode;
-	}
-
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getFax() {
-		return fax;
-	}
-
-	public void setFax(String fax) {
-		this.fax = fax;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getCreatorId() {
-		return creatorId;
-	}
-
-	public void setCreatorId(String creatorId) {
-		this.creatorId = creatorId;
-	}
-
-	public String getLastUpdatorId() {
-		return lastUpdatorId;
-	}
-
-	public void setLastUpdatorId(String lastUpdatorId) {
-		this.lastUpdatorId = lastUpdatorId;
-	}
-
-	public String getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(String createDate) {
-		this.createDate = createDate;
-	}
-
-	public String getLastUpdateDate() {
-		return lastUpdateDate;
-	}
-
-	public void setLastUpdateDate(String lastUpdateDate) {
-		this.lastUpdateDate = lastUpdateDate;
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [_id=" + _id + ", internalId=" + customerId + ", name=" + name + ", addressLine1="
-				+ addressLine1 + ", addressLine2=" + addressLine2 + ", addressLine3=" + addressLine3 + ", city=" + city
-				+ ", state=" + state + ", postalCode=" + postalCode + ", country=" + country + ", phone=" + phone
-				+ ", fax=" + fax + ", email=" + email + ", creatorId=" + creatorId + ", lastUpdatorId=" + lastUpdatorId
-				+ ", createDate=" + createDate + ", lastUpdateDate=" + lastUpdateDate + "]";
-	}
+@Override
+public String toString() {
+return "Customer [_id=" + _id + ", customerId=" + customerId + ", name=" + name + ", addressLine1="
++ addressLine1 + ", addressLine2=" + addressLine2 + ", addressLine3=" + addressLine3 + ", city=" + city
++ ", state=" + state + ", postalCode=" + postalCode + ", country=" + country + ", phone=" + phone
++ ", fax=" + fax + ", email=" + email + ", creatorId=" + creatorId + ", lastUpdatorId=" + lastUpdatorId
++ ", createDate=" + createDate + ", lastUpdateDate=" + lastUpdateDate + "]";
 }
-/**
-{
-    "INTERNAL_ID" : NumberInt(45),
-    "NAME" : "ConAgra",
-    "ADDRESS_LINE_1" : "5645 N 90TH STREETs",
-    "ADDRESS_LINE_2" : "MAILSTOP 90-180",
-    "ADDRESS_LINE_3" : "",
-    "CITY" : "OMAHA",
-    "STATE" : "NE",
-    "POSTAL_CODE" : NumberInt(68134),
-    "COUNTRY" : "USA",
-    "PHONE" : "847-303-2722",
-    "FAX" : "18473037674",
-    "EMAIL" : "",
-    "CREATOR_ID" : "POCSLV",
-    "LAST_UPDATER_ID" : "POCSLV",
-    "CREATE_DATE" : "01-JAN-00 12.59.59.000000000 AM",
-    "LAST_UPDATE_DATE" : "01-JAN-00 09.59.59.000000000 PM"
 }
-**/
