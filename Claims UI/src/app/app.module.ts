d5ae4511d61c015aa716169fe47bbbff04ac19d8
hmalipatlolla <119707079+hmalipatlolla@users.noreply.github.com>
2022-12-21 09:36:45 -0600

    code refactoring

--- Claims UI/src/app/app.module.ts
+++ Claims UI/src/app/app.module.ts
@@ -1,52 +1,50 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddClaimComponent, BottomSheetOverviewExampleSheet, DialogBoxComponent } from './components/add-claims/add-claims.component';
 import { DataTableComponent, DataTableOrdersComponent } from './components/data-table/data-table.component';
-import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './auth.guard';
 import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataCardsComponent } from './components/data-cards/data-cards.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatCardModule} from '@angular/material/card';
+import { BrowserModule } from '@angular/platform-browser';
 import { ChartsModule } from 'ng2-charts';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSelectModule} from '@angular/material/select';
-
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {NgxPrintModule} from 'ngx-print';
-import {MatNativeDateModule} from '@angular/material/core';
-import {HttpClientModule} from '@angular/common/http';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { AddClaimComponent, BottomSheetOverviewExampleSheet, DialogBoxComponent } from './components/add-claims/add-claims.component';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import { LoginComponent } from './components/login/login.component';
-import { DetailsModalComponent } from './components/data-table/details-modal/details-modal.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatTooltipModule} from '@angular/material/tooltip';
 import { ClaimsApiService } from './claims-api.service';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { ClaimsDetailsComponent } from './components/claims-details/claims-details.component';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatGridListModule} from '@angular/material/grid-list';
-import { AuthGuard } from './auth.guard';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DataCardsComponent } from './components/data-cards/data-cards.component';
+import { DetailsModalComponent } from './components/data-table/details-modal/details-modal.component';
+import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NgModule } from '@angular/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { NgxPrintModule } from 'ngx-print';
 
 @NgModule({
   declarations: [
@@ -86,19 +84,20 @@ import { AuthGuard } from './auth.guard';
     HttpClientModule,
     MatNativeDateModule,
     MatInputModule,
-    MatDatepickerModule,MatBottomSheetModule,
-   NgxDatatableModule,
-   MatSlideToggleModule,
-   NgxPrintModule,
-   MatDialogModule,
-   MatProgressBarModule,
-   MatTooltipModule,
-   MatAutocompleteModule,
-   MatSnackBarModule,
-   MatProgressSpinnerModule,
-   HttpClientModule,
-   MatGridListModule,
-   MatSelectModule
+    MatDatepickerModule,
+    MatBottomSheetModule,
+    NgxDatatableModule,
+    MatSlideToggleModule,
+    NgxPrintModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatTooltipModule,
+    MatAutocompleteModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatSelectModule
   ],
   providers: [ClaimsApiService,AuthGuard],
   bootstrap: [AppComponent]
